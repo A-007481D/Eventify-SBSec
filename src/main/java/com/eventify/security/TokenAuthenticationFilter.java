8f999d59bf1415677cd5e6f609210611e86edab3
A-007481D <labidabdelmalek@gmail.com>
2025-11-17 15:09:39 +0100

    feat: add token auth filter

--- src/main/java/com/eventify/security/TokenAuthenticationFilter.java
+++ src/main/java/com/eventify/security/TokenAuthenticationFilter.java
@@ -1,20 +1,48 @@
 package com.eventify.security;
 
 
+import com.eventify.model.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private final TokenService tokenService;
+
+    public TokenAuthenticationFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
 
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = response.encodeURL(request.getHeader("Authorization"));
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+
+            TokenService.UserInfo userInfo = tokenService.decryptToken(token);
+            if (userInfo != null) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userInfo.email, null, Collections.singletonList(new SimpleGrantedAuthority(userInfo.role)));
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
 }
