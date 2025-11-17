7d36b651b34de0f7dbfcdb8fdcc43dccece5c4eb
A-007481D <labidabdelmalek@gmail.com>
2025-11-17 15:19:07 +0100

    feat: add the DTOs and user repository along with custom security layer classes

--- src/main/java/com/eventify/security/CustomAuthenticationProvider.java
+++ src/main/java/com/eventify/security/CustomAuthenticationProvider.java
@@ -1,19 +1,46 @@
 package com.eventify.security;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomAuthenticationProvider  implements AuthenticationProvider {
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(CustomUserDetailsService userDetailsService,
+                                        PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    email,
+                    password,
+                    userDetails.getAuthorities()
+            );
+        } else {
+            throw new BadCredentialsException("Invalid credentials");
+        }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
